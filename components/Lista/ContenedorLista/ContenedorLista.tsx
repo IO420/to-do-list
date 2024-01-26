14d86e8b041006031f4ed9b2fe1be1c9f585e76d
IO <IO@IO.com>
2024-01-26 01:18:58 -0600

    to do list terminada v1.0

--- components/Lista/ContenedorLista/ContenedorLista.tsx
+++ components/Lista/ContenedorLista/ContenedorLista.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import ItemLista from "../Item/ItemLista";
 import style from "@/styles/lista.module.css";
@@ -20,11 +20,30 @@ const listaArray: item[] = [
     color: "green",
     hecho: true,
   },
-
+  {
+    id: 2,
+    titulo: "Titulo 2",
+    descripcion: "Descripcion 2",
+    color: "blue",
+    hecho: false,
+  },
+  {
+    id: 3,
+    titulo: "Titulo 3",
+    descripcion: "Descripcion 3",
+    color: "red",
+    hecho: false,
+  },
+  {
+    id: 4,
+    titulo: "Titulo",
+    descripcion: "Descripcion 4",
+    color: "purple",
+    hecho: true,
+  },
 ];
 
 export default function () {
-
   const [lista, setLista] = useState<item[]>(listaArray);
   const [newItem, setNewItem] = useState<item>({
     color: "#000000",
@@ -33,7 +52,6 @@ export default function () {
     id: -1,
     titulo: "",
   });
-
   const [isShowForm, setIsShowForm] = useState<boolean>(false);
 
   const handleShowForm = (): void => {
@@ -56,24 +74,32 @@ export default function () {
     setLista(lista.filter((item) => item.id !== id));
   };
 
-  const handleOnChecked = (id: number) => {
-    console.log(id);
+  const handleOnChecked = (itemToChange: item) => {
+    setLista(
+      lista.map((item) => {
+        if (item.id === itemToChange.id) {
+          return itemToChange;
+        } else {
+          return item;
+        }
+      })
+    );
   };
 
   return (
     <>
       <div className="shadow-sm  bg-white rounded p-3">
         {isShowForm ? (
-      <Formulario
+          <Formulario
             lista={lista}
             newItem={newItem}
             setNewItem={setNewItem}
             handleShowForm={handleShowForm}
             handleAddNewItem={handleAddNewItem}
-      />
-        ):(
-      <ul className={`list-unstyled m-0 d-flex flex-column ${style.lista}`}>
-          {lista ? (
+          />
+        ) : (
+          <ul className={`list-unstyled m-0 d-flex flex-column ${style.lista}`}>
+            {lista ? (
               lista.length > 0 ? (
                 lista.map((item, index) => (
                   <ItemLista
@@ -82,15 +108,15 @@ export default function () {
                     onDeleteItem={handleDeleteItem}
                     onChecked={handleOnChecked}
                   />
-          ))
-          ):(
-            <>La lista está vacia</>
+                ))
+              ) : (
+                <>La lista está vacia</>
               )
             ) : (
               <>Lista is undefined</>
-          )}
-        </ul>
-      )}
+            )}
+          </ul>
+        )}
       </div>
       {isShowForm ? (
         <></>
@@ -103,13 +129,6 @@ export default function () {
           Crear item
         </button>
       )}
-      <button
-        type="button"
-        className="shadow-sm bg-white rounded mt-3 p-3  border-0 mb-5"
-        onClick={() => console.log(lista)}
-      >
-        Imprimir lista
-      </button>
     </>
   );
 }
--- components/Lista/Item/ItemLista.tsx
+++ components/Lista/Item/ItemLista.tsx
@@ -1,44 +1,50 @@
-import React from "react";
+import React, { Dispatch, SetStateAction, useState } from "react";
 import { item } from "../ContenedorLista/ContenedorLista";
 
 interface ItemListaProps {
   item: item;
   onDeleteItem: (id: number) => void;
-  onChecked: (id: number) => void;
+  onChecked: (item: item) => void;
 }
 
-export default function ItemLista({ item, onDeleteItem,
-    onChecked }: ItemListaProps) {
+export default function ItemLista({
+  item,
+  onDeleteItem,
+  onChecked,
+}: ItemListaProps) {
+  const [isShowDescription, setIsShowDescription] = useState<boolean>(false);
+
   return (
-
-        <li className="d-flex justify-content-between " 
-            style={{ 
-            minWidth: "300px", 
-            maxWidth: "400px", 
-            cursor: "pointer" 
+    <li
+      className="d-flex justify-content-between "
+      style={{ minWidth: "300px", maxWidth: "400px", cursor: "pointer" }}
+    >
+      <div
+        className="p-3 rounded w-100"
+        style={{
+          border: "1px solid #cecece",
+          borderLeft: `10px solid ${item.color}`,
         }}
+        onClick={() => setIsShowDescription(!isShowDescription)}
+      >
+        <h5 className="m-0 ">{item.titulo}</h5>
+        {isShowDescription && <p className="m-0">{item.descripcion}</p>}
+      </div>
+      <div className="d-flex align-items-center ">
+        <input
+          className="ms-3 form-check-input fs-3"
+          type="checkbox"
+          checked={item.hecho}
+          onChange={() => onChecked({ ...item, hecho: !item.hecho })}
+        />
+        <button
+          type="button"
+          className="btn btn-danger ms-3"
+          onClick={() => onDeleteItem(item.id)}
         >
-            <div
-                className="p-3 rounded w-100"
-                style={{
-                border: "1px solid #cecece",
-                borderLeft: `10px solid ${item.color}`
-            }}
-            >
-                <h5 className="m-0 ">{item.titulo}</h5>
-            </div>
-            <div className="d-flex align-items-center ">
-                <input
-                    className="ms-3 form-check-input fs-3"
-                    type="checkbox"
-                    checked={item.hecho}
-                    onChange={() => onChecked(item.id)}
-                />
-                <button type="button" className="btn btn-danger ms-3" onClick={() => onDeleteItem(item.id)}>
-                    Borrar
-                </button>
-            </div>
-        </li>
-
+          Borrar
+        </button>
+      </div>
+    </li>
   );
 }
